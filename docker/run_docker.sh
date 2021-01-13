05b98ccd0e98289b880882203ef56d1d17e01e6c
Weijie Liu <lpjlwjlpjlwj@126.com>
2021-01-13 12:48:11 -0500

    Update run_docker.sh

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,7 +1,7 @@
 export LD_LIBRARY_PATH=$AEROOT/setup/dyninst/install/lib:$AEROOT/setup/libunwind/install/lib:$LD_LIBRARY_PATH
 export LD_PRELOAD=$AEROOT/setup/dyninst/install/lib/libdyninstAPI_RT.so:$AEROOT/setup/libunwind/install/lib/libunwind.so
-docker pull ubuntu:18.04
-docker images
-docker run --name ubuntu18 -d ubuntu:18.04
-docker ps
-docker exec -it ubuntu18 bash
+./docker.inst pull ubuntu:18.04
+./docker.inst images
+./docker.inst run --name ubuntu18 -d ubuntu:18.04
+./docker.inst ps
+./docker.inst exec -it ubuntu18 bash
