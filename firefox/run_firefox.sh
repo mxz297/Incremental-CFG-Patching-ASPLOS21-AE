08fbed5c63523e80dd4046340ec8504ad779a1c7
Xiaozhu Meng <mxz297@gmail.com>
2021-01-13 10:27:14 -0600

    Update run scripts to setup environment variables

--- firefox/run_firefox.sh
+++ firefox/run_firefox.sh
@@ -1,7 +1,3 @@
 export LD_LIBRARY_PATH=$AEROOT/setup/dyninst/install/lib:$AEROOT/setup/libunwind/install/lib:$LD_LIBRARY_PATH
 export LD_PRELOAD=$AEROOT/setup/dyninst/install/lib/libdyninstAPI_RT.so:$AEROOT/setup/libunwind/install/lib/libunwind.so
-docker pull ubuntu:18.04
-docker images
-docker run --name ubuntu18 -d ubuntu:18.04
-docker ps
-docker exec -it ubuntu18 bash
+firefox
