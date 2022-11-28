7c59b934c73065192ec8b87eef5e93235e303257
Weijie Liu <lpjlwjlpjlwj@126.com>
2022-11-28 15:47:41 +0800

    Update run_docker.sh
    add 'docker cp' test

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -4,4 +4,6 @@ export LD_PRELOAD=$AEROOT/setup/dyninst/install/lib/libdyninstAPI_RT.so:$AEROOT/
 ./docker.inst images
 ./docker.inst run --name ubuntu18 -d ubuntu:18.04 bash
 ./docker.inst ps
+if=/dev/urandom of=file.txt bs=1M count=10
+./docker.inst cp file.txt ubuntu18:/tmp 
 ./docker.inst exec -it ubuntu18 bash
